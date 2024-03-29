173bca5d9d15de393dcfe08d9637322f0962f9b4
pond7897 <80212418+pond7897@users.noreply.github.com>
2024-03-30 03:51:39 +0700

    test

--- app/main/historys/page.jsx
+++ app/main/historys/page.jsx
@@ -67,7 +67,7 @@ export default function HistoryPage() {
           className="bg-[#F97316] text-white"
           style={{ fontFamily: "Kanit" }}
         >
-          วันที่ยืม/วันที่คืนเมื่อไหร่
+          วันที่ยืม/วันที่คืน
         </TableColumn>
         <TableColumn
           key="detail"
