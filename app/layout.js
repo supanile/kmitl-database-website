08841d027eec7a49607b4c266b964e6dc07f594c
pond7897 <80212418+pond7897@users.noreply.github.com>
2024-03-30 03:48:56 +0700

    test commit

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './ui/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'KMITL Equipment Borrow System',
+  title: 'KMITL Equipment Borrow Systemmmm',
   description: 'Next.js 14',
 }
 
