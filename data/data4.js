e919c398a525fec2ed7464fd4bec6e03ed19bd3f
supawitnile <83073023+supanile@users.noreply.github.com>
2024-04-01 03:04:13 +0700

    Add files via upload

---
+++ data/data4.js
@@ -0,0 +1,107 @@
+export const users = [
+    {
+        "fdId": 1,
+        "fdAssetId": "CP-0001",
+        "fdAssetName": "คอมพิวเตอร์ acer",
+        "fdCategoryId": 2,
+        "fdStatus": 0
+    },
+    {
+        "fdId": 2,
+        "fdAssetId": "CP-0002",
+        "fdAssetName": "คอมพิวเตอร์ asus",
+        "fdCategoryId": 2,
+        "fdStatus": 1
+    },
+    {
+        "fdId": 3,
+        "fdAssetId": "CP-0003",
+        "fdAssetName": "คอมพิวเตอร์ acer",
+        "fdCategoryId": 2,
+        "fdStatus": 1
+    },
+    {
+        "fdId": 4,
+        "fdAssetId": "CP-0004",
+        "fdAssetName": "เมาส์",
+        "fdCategoryId": 2,
+        "fdStatus": 0
+    },
+    {
+        "fdId": 5,
+        "fdAssetId": "CP-0005",
+        "fdAssetName": "เมนบอร์ด",
+        "fdCategoryId": 2,
+        "fdStatus": 0
+    },
+    {
+        "fdId": 6,
+        "fdAssetId": "CP-0006",
+        "fdAssetName": "แผ่นซีดี",
+        "fdCategoryId": 2,
+        "fdStatus": 1
+    },
+    {
+        "fdId": 7,
+        "fdAssetId": "SP-0001",
+        "fdAssetName": "ลูกฟุตบอล",
+        "fdCategoryId": 1,
+        "fdStatus": 1
+    },
+    {
+        "fdId": 8,
+        "fdAssetId": "SP-0002",
+        "fdAssetName": "ลูกฟุตบอล",
+        "fdCategoryId": 1,
+        "fdStatus": 0
+    },
+    {
+        "fdId": 9,
+        "fdAssetId": "SP-0003",
+        "fdAssetName": "ไม้ตีปิงปอง",
+        "fdCategoryId": 1,
+        "fdStatus": 0
+    },
+    {
+        "fdId": 10,
+        "fdAssetId": "SP-0004",
+        "fdAssetName": "ไม้ตีปิงปอง",
+        "fdCategoryId": 1,
+        "fdStatus": 0
+    },
+    {
+        "fdId": 11,
+        "fdAssetId": "CM-0001",
+        "fdAssetName": "เครื่องวัดความเป็นกรด-ด่างแบบตั้งโต๊ะ",
+        "fdCategoryId": 3,
+        "fdStatus": 1
+    },
+    {
+        "fdId": 12,
+        "fdAssetId": "CM-0002",
+        "fdAssetName": "เครื่องวิเคราะห์สาร",
+        "fdCategoryId": 3,
+        "fdStatus": 1
+    },
+    {
+        "fdId": 13,
+        "fdAssetId": "CM-0003",
+        "fdAssetName": "เครื่องวัดความดันบรรยากาศ",
+        "fdCategoryId": 3,
+        "fdStatus": 1
+    },
+    {
+        "fdId": 14,
+        "fdAssetId": "CM-0004",
+        "fdAssetName": "ชุดเครื่องแก้ว",
+        "fdCategoryId": 3,
+        "fdStatus": 0
+    },
+    {
+        "fdId": 15,
+        "fdAssetId": "CM-0005",
+        "fdAssetName": "ชุดแบบจำลองทางเคมี",
+        "fdCategoryId": 3,
+        "fdStatus": 0
+    }
+]
